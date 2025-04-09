a67bb5ffb7ba02a2f34c338d0cf8df945a17bb7b
Ruslan <sakhrus14061999@gmail.com>
2025-04-09 09:59:40 +0300

    item errror handled

--- client/src/pages/SinglePage.tsx
+++ client/src/pages/SinglePage.tsx
@@ -10,23 +10,44 @@ type ItemType = {
 function SinglePage() {
   const { id } = useParams();
   const [item, setItem] = useState<ItemType | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  
 
   useEffect(() => {
     fetch(`${process.env.API_URL}/items/${id}`)
-      .then(res => res.json())
+      .then(res => {
+        if(!res.ok) {
+          throw new Error(`Failed to fetch ${id}`)
+        }
+        return res.json()
+      })
       .then(data => setItem(data))
       .catch(err => {
         console.error('Failed to fetch item', err);
-      });
-  }, []);
+        setError('Cannot load item data')
+      })
+      .finally(() => setLoading(false))
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+  if (!item) {
+    return <div>No item found.</div>;
+  }
 
   return (
     <div className="detail">
         <Link to={'/'}>Go Back</Link>
       <h2>Item Details</h2>
-      <p>ID: {item!.id}</p>
-      <p>Name: {item!.name}</p>
-      <p>Description: {item!.description}</p>
+      <p>ID: {item.id}</p>
+      <p>Name: {item.name}</p>
+      <p>Description: {item.description}</p>
     </div>
   );
 }
